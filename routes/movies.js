ecd52ae9181640c0ecfe21f75528e66be19d6b4d
Debasis Saikia <debasishsaikia136@gmail.com>
2020-11-12 20:20:33 +0530

    'movie_route_modified'

--- routes/movies.js
+++ routes/movies.js
@@ -43,22 +43,106 @@ router.post("/", async (req, res) => {
 
   try {
     const newMovie = await movie.save();
-    res.redirect("movies");
+    res.redirect(`movies/${newMovie.id}`);
   } catch (err) {
     console.log(err);
     renderNewPage(res, movie, true);
   }
 });
 
+// show movie route:
+router.get("/:id", async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id)
+      .populate("director")
+      .exec();
+    res.render("movies/show", { movie: movie });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+// Edit movie
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    renderEditPage(res, movie);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+// update movie
+router.put("/:id", async (req, res) => {
+  let movie;
+  try {
+    const { title, director, ratingCount, description } = req.body;
+    movie = await Movie.findById(req.params.id);
+    movie.title = title;
+    movie.director = director;
+    movie.releaseDate = new Date(req.body.releaseDate);
+    movie.ratingCount = ratingCount;
+    movie.description = description;
+
+    if (req.body.cover != null && req.body.cover !== "") {
+      saveCover(movie, req.body.cover);
+    }
+    await movie.save();
+    res.redirect(`/movies/${movie.id}`);
+  } catch {
+    if (movie != null) {
+      renderEditPage(res, movie, true);
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
+// delete routes
+router.delete("/:id", async (req, res) => {
+  let movie;
+  try {
+    movie = await Movie.findById(req.params.id);
+    await movie.remove();
+    res.redirect("/movies");
+  } catch {
+    if (movie != null) {
+      res.render("movies/show", {
+        movie: movie,
+        errorMessage: "Could not Delete book !! Try again",
+      });
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
 async function renderNewPage(res, movie, isError = false) {
+  renderFormPage(res, movie, "new", isError);
+}
+
+async function renderEditPage(res, movie, isError = false) {
+  renderFormPage(res, movie, "edit", isError);
+}
+
+async function renderFormPage(res, movie, form, isError = false) {
   try {
     const directors = await Director.find({});
     const params = {
       directors: directors,
       movie: movie,
     };
-    if (isError) params.errorMessage = "Error in creating Book";
-    res.render("movies/new", params);
+    if (isError) {
+      if (form === "edit") {
+        params.errorMessage = "Error in updating Movie";
+      } else {
+        params.errorMessage = "Error creating Movie";
+      }
+    }
+    if (isError) params.errorMessage = "Error in creating Movie";
+    res.render(`movies/${form}`, params);
   } catch {
     res.redirect("/movies");
   }
